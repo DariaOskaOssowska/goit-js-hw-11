2cdd5370315365f963f0282d67d2f549cf728555
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-08-31 23:23:34 +0200

    Galeria i obraz karty

---
+++ src/js/fetchImages.js
@@ -0,0 +1,17 @@
+export const fetchImages = (inputValue, pageNr) => {
+  return fetch(
+    `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          return [];
+        }
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
---
+++ src/js/index.js
@@ -0,0 +1,83 @@
+import { fetchImages } from '../js/fetchImages';
+import Notiflix from 'notiflix';
+// // Opisany w dokumentacji
+// import SimpleLightbox from 'simplelightbox';
+// // Dodatkowy import stylów
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const input = document.querySelector('.search-form-input');
+const btnSearch = document.querySelector('.search-form-button');
+const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
+
+let pageNumber = 1;
+
+btnSearch.addEventListener('click', e => {
+  e.preventDefault();
+  cleanGallery();
+  const trimmedValue = input.value.trim();
+  console.log(fetchImages(trimmedValue, pageNumber), 'Siemanko fetch');
+
+  fetchImages(trimmedValue, pageNumber).then(foundData => {
+    if (foundData.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      renderImageList(foundData.hits);
+      Notiflix.Notify.success(
+        `Hooray! We found ${foundData.totalHits} images.`
+      );
+    }
+  });
+});
+
+btnLoadMore.addEventListener('click', () => {
+  pageNumber++;
+const trimmedValue = input.value.trim();
+console.log(fetchImages(trimmedValue, pageNumber), 'Siemanko fetch');
+
+fetchImages(trimmedValue, pageNumber).then(foundData => {
+  if (foundData.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    renderImageList(foundData.hits);
+    Notiflix.Notify.success(`Hooray! We found ${foundData.totalHits} images.`);
+  }
+});
+
+});
+
+function renderImageList(images) {
+  console.log(images, 'images');
+  const markup = images
+    .map(image => {
+      console.log('img', image);
+      return `<div class="photo-card">
+       <img class="photo" src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
+        <div class="info">
+           <p class="info-item">
+    <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+</p>
+            <p class="info-item">
+                <b>Views</b> <span class="info-item-api">${image.views}</span>  
+            </p>
+            <p class="info-item">
+                <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
+            </p>
+            <p class="info-item">
+                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
+            </p>
+        </div>
+    </div>`;
+    })
+    .join('');
+  gallery.innerHTML = markup;
+}
+
+function cleanGallery() {
+  gallery.innerHTML = '';
+  pageNumber = 1;
+}
