0503c0399366b8a7d5cb795dce06cf2ddb801697
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-09-02 20:48:28 +0200

    Dodanie biblioteki SimpleLightbox

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,17 @@ const input = document.querySelector('.search-form-input');
 const btnSearch = document.querySelector('.search-form-button');
 const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
+
+const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+
+  behavior: 'smooth',
+});
 
 btnLoadMore.style.display = 'none';
 
@@ -28,6 +39,7 @@ btnSearch.addEventListener('click', e => {
           `Hooray! We found ${foundData.totalHits} images.`
         );
         btnLoadMore.style.display = 'block';
+        gallerySimpleLightbox.refresh();
       }
     });
   }
@@ -58,7 +70,9 @@ function renderImageList(images) {
     .map(image => {
       console.log('img', image);
       return `<div class="photo-card">
-       <img class="photo" src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
+
+       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+
         <div class="info">
            <p class="info-item">
     <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
