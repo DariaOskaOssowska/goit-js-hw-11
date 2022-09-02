789dacccec84aaa6a346736ca6da3d36a1085072
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-09-02 19:06:49 +0200

    Update

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -2,14 +2,14 @@ export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
     `https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
-    .then(response => {
+    .then(async response => {
       if (!response.ok) {
         if (response.status === 404) {
           return [];
         }
         throw new Error(response.status);
       }
-      return response.json();
+      return await response.json();
     })
     .catch(error => {
       console.error(error);
--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,27 @@ btnSearch.addEventListener('click', e => {
   e.preventDefault();
   cleanGallery();
   const trimmedValue = input.value.trim();
+  if (trimmedValue !== '') {
+    fetchImages(trimmedValue, pageNumber).then(foundData => {
+      if (foundData.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderImageList(foundData.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${foundData.totalHits} images.`
+        );
+        btnLoadMore.style.display = 'block';
+      }
+    });
+  }
+});
 
+btnLoadMore.addEventListener('click', () => {
+  pageNumber++;
+  const trimmedValue = input.value.trim();
+  btnLoadMore.style.display = 'none';
   fetchImages(trimmedValue, pageNumber).then(foundData => {
     if (foundData.hits.length === 0) {
       Notiflix.Notify.failure(
@@ -28,25 +48,6 @@ btnSearch.addEventListener('click', e => {
         `Hooray! We found ${foundData.totalHits} images.`
       );
       btnLoadMore.style.display = 'block';
-    }
-  });
-});
-
-btnLoadMore.addEventListener('click', () => {
-  pageNumber++;
-  const trimmedValue = input.value.trim();
-
-  fetchImages(trimmedValue, pageNumber).then(foundData => {
-    if (foundData.hits.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      btnLoadMore.style.display = 'none';
-    } else {
-      renderImageList(foundData.hits);
-      Notiflix.Notify.success(
-        `Hooray! We found ${foundData.totalHits} images.`
-      );
     }
   });
 });
@@ -81,4 +82,5 @@ function renderImageList(images) {
 function cleanGallery() {
   gallery.innerHTML = '';
   pageNumber = 1;
+  btnLoadMore.style.display = 'none';
 }
